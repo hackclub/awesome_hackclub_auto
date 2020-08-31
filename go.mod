ffc972a6aeb4c4e6146cd979e594e61a3db23f94
Caleb Denio <cjdenio44@gmail.com>
2020-08-31 16:33:53 -0400

    🗃 Add Insert function to DB

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.15
 
 require (
 	github.com/Matt-Gleich/logoru v0.0.0-20200817001912-097ac1695c5f
+	github.com/google/uuid v1.1.2
 	github.com/gorilla/mux v1.8.0
 	github.com/slack-go/slack v0.6.6
 	golang.org/x/sys v0.0.0-20200828194041-157a740278f4 // indirect
--- pkg/db/couchdb/couchdb.go
+++ pkg/db/couchdb/couchdb.go
@@ -9,12 +9,15 @@ import (
 	"io/ioutil"
 	"net/http"
 	"time"
+
+	"github.com/google/uuid"
 )
 
 type Client struct {
-	URI      string
-	user     string
-	password string
+	URI        string
+	user       string
+	password   string
+	httpClient http.Client
 }
 
 type Database struct {
@@ -27,6 +30,9 @@ func NewClient(uri, user, password string) *Client {
 		URI:      uri,
 		user:     user,
 		password: password,
+		httpClient: http.Client{
+			Timeout: 10 * time.Second,
+		},
 	}
 }
 
@@ -43,10 +49,7 @@ func (database *Database) Get(id string, v interface{}) error {
 		return err
 	}
 	req.Header.Add("Authorization", database.client.authHeader())
-	httpClient := http.Client{
-		Timeout: 10 * time.Second,
-	}
-	res, err := httpClient.Do(req)
+	res, err := database.client.httpClient.Do(req)
 	if err != nil {
 		return err
 	}
@@ -66,6 +69,36 @@ func (database *Database) Get(id string, v interface{}) error {
 	}
 
 	return nil
+}
+
+func (database *Database) Insert(v interface{}) (string, error) {
+	docID := uuid.New().String()
+
+	body, err := json.Marshal(v)
+	if err != nil {
+		return "", err
+	}
+
+	req, err := http.NewRequest("PUT", fmt.Sprintf("%s/%s/%s", database.client.URI, database.Name, docID), bytes.NewBuffer(body))
+	if err != nil {
+		return "", err
+	}
+	req.Header.Add("Authorization", database.client.authHeader())
+	req.Header.Add("Content-Type", "application/json")
+	res, err := database.client.httpClient.Do(req)
+	if err != nil {
+		return "", err
+	}
+
+	if res.StatusCode < 200 || res.StatusCode > 299 {
+		result, err := ioutil.ReadAll(res.Body)
+		if err != nil {
+			return "", err
+		}
+		return "", errors.New(string(result))
+	}
+
+	return docID, nil
 }
 
 func (client *Client) authHeader() string {
