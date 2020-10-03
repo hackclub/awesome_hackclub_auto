04196f6cd20a2378b23669db5c1d6d72b6ed548b
Caleb Denio <cjdenio44@gmail.com>
2020-10-03 18:03:42 -0400

    🔊 Only report to Honeybadger on error

--- pkg/logging/logging.go
+++ pkg/logging/logging.go
@@ -9,26 +9,27 @@ import (
 	"github.com/honeybadger-io/honeybadger-go"
 )
 
-func Log(v interface{}, level string, local bool) {
-	if os.Getenv("HONEYBADGER_API_KEY") != "" && !local {
+func Log(v interface{}, level string, localOnly bool) {
+	// Only log to Honeybadger if an API key is set and it's an error
+	if os.Getenv("HONEYBADGER_API_KEY") != "" && !localOnly && (level == "error" || level == "critical" || level == "warning") {
 		_, err := honeybadger.Notify(fmt.Sprintf("%s: %v", strings.ToUpper(level), v))
 		if err != nil {
 			logoru.Error("Ironically enough, something went wrong while trying to log an error :(")
 		}
-	} else {
-		switch level {
-		case "debug":
-			logoru.Debug(v)
-		case "info":
-			logoru.Info(v)
-		case "warning":
-			logoru.Warning(v)
-		case "error":
-			logoru.Error(v)
-		case "critical":
-			logoru.Critical(v)
-		case "success":
-			logoru.Success(v)
-		}
+	}
+
+	switch level {
+	case "debug":
+		logoru.Debug(v)
+	case "info":
+		logoru.Info(v)
+	case "warning":
+		logoru.Warning(v)
+	case "error":
+		logoru.Error(v)
+	case "critical":
+		logoru.Critical(v)
+	case "success":
+		logoru.Success(v)
 	}
 }
